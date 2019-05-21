af2c669f02bbe87c2f8dc8a0647e3b5115fa2137
Wangzihao5325 <416990273@qq.com>
2019-05-21 17:05:57 +0700

    1.normal merge

--- src/screen/inviteCode/index.js
+++ src/screen/inviteCode/index.js
@@ -4,7 +4,9 @@ export default class InviteCodeModel extends Component {
 
     render() {
         return (
-            <div style={{ height: 100, width: 100, backgroundColor: 'white' }}>
+            <div style={{ height: 386, width: 290, backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div style={{ color: 'rgb(34,34,34)', fontSize: 18, marginTop: 31 }}>我的邀请码</div>
+                <div style={{ color: 'rgb(255,42,49)', fontSize: 23, marginTop: 20 }}>12345</div>
 
             </div>
         );
