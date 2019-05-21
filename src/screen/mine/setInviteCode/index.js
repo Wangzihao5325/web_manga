2a55522704d30e305ab84fead56a93d5cb929d89
Wangzihao5325 <416990273@qq.com>
2019-05-21 22:20:41 +0700

    1.增加邀请码页面

--- src/screen/mine/setInviteCode/index.js
+++ src/screen/mine/setInviteCode/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import store from '../../../store/index';
+import { tab_navi_unshow } from '../../../store/actions/tabBottomNaviAction';
+
 import { HeaderPro } from '../../../component/header/index';
 
-export default class SetInviteCode extends Component {
+class SetInviteCode extends Component {
+
+    componentDidMount() {
+        store.dispatch(tab_navi_unshow());
+    }
+
     render() {
         return (
             <div style={{ flex: 1 }}>
@@ -10,4 +19,12 @@ export default class SetInviteCode extends Component {
             </div>
         );
     }
+
+    goBack = () => {
+        this.props.history.push('/mine/');
+    }
+
 }
+
+const SetInviteCodeWithRouter = withRouter(SetInviteCode);
+export default SetInviteCodeWithRouter;
