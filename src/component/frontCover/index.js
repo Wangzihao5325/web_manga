215da7b469939849beffdc43066197f157517d70
Wangzihao5325 <416990273@qq.com>
2019-05-27 20:18:49 +0700

    1.normal merge

--- src/component/frontCover/index.js
+++ src/component/frontCover/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Rate } from 'antd';
 import { CLIENT_HEIGHT, CLIENT_WIDTH } from '../../global/sizes';
 import SecurtyImage from '../../component/securtyImage/Image';
 import './index.css';
+import indexTabImage from '../../image/main/index_tab.png';
 
 const VER_WIDTH = (CLIENT_WIDTH - 9 * 2 - 12 * 2) / 3;
 const VER_IMAGE_HEIGHT = VER_WIDTH * 1.4;
@@ -74,14 +77,59 @@ class BannerCover extends Component {
     }
 }
 
+const COMIC3_ITEM_WIDTH = CLIENT_WIDTH - 24;
+const COMIC3_ITEM_HEIGHT = COMIC3_ITEM_WIDTH / 2;
+const COMIC3_ITEM_IMAGE_HEIGHT = COMIC3_ITEM_HEIGHT - 30;
+const COMIC3_ITEM_IMAGE_WIDTH = COMIC3_ITEM_IMAGE_HEIGHT / 1.5;
+
+class Comic3Item extends Component {
+    render() {
+        return (
+            <div style={{ width: COMIC3_ITEM_WIDTH, height: COMIC3_ITEM_HEIGHT, display: 'flex', flexDirection: 'row', backgroundColor: 'rgb(249,249,249)', borderRadius: 4 }}>
+                <div className='bg-image-container' style={{ position: 'absolute', top: 6, left: 24, height: 28, width: 25, backgroundImage: `url(${indexTabImage})`, display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', marginTop: 5, fontSize: 12, fontWeight: 'bold' }}>
+                    1
+                </div>
+                <div style={{ marginLeft: 18, alignSelf: 'center', display: 'flex' }}>
+                    <SecurtyImage borderRadius={5} style={{ width: COMIC3_ITEM_IMAGE_WIDTH, height: COMIC3_ITEM_IMAGE_HEIGHT }} source={this.props.source} />
+                </div>
+                <div style={{ marginRight: 18, marginLeft: 21, flex: 1, height: COMIC3_ITEM_IMAGE_HEIGHT, display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+                    <div className='text_div' style={{ fontSize: 15, color: 'rgb(34,34,34)', fontWeight: 'bold', marginTop: 3 }}>瑜珈人妻的濕熱呻吟</div>
+                    <div style={{ height: 20, display: 'flex', flexDirection: 'row', backgroundColor: 'yellow' }}>
+
+                    </div>
+                    <div style={{ marginTop: 5, height: 30, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <Rate disabled defaultValue={2} />
+                        <div style={{ fontSize: 18, color: 'rgb(168,168,168)', fontWeight: 'bold' }}>10</div>
+                    </div>
+                    <div style={{ marginTop: 5, height: 20, display: 'flex', flexDirection: 'row', fontSize: 13, color: 'rgb(168,168,168)' }}>
+                        更新至108话
+                    </div>
+                    <div style={{ marginTop: 5, height: 20, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <div><img style={{ height: 17, width: 17 }} src={require('../../image/main/first_fire.png')} alt='' /></div>
+                        <div style={{ fontSize: 16, color: 'rgb(255,42,49)', marginLeft: 6 }}>1234</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    _typeGen = () => {
+
+    }
+}
+
 export {
     FrontCover,
     FrontCoverHo,
     BannerCover,
+    Comic3Item,
+
     VER_WIDTH,
     VER_HEIGHT,
     HO_WIDTH,
     HO_HEIGHT,
     BANNER_WIDTH,
-    BANNER_TOTAL_HEIGHT
+    BANNER_TOTAL_HEIGHT,
+    COMIC3_ITEM_WIDTH,
+    COMIC3_ITEM_HEIGHT,
 }
--- src/component/modal/Comic.js
+++ src/component/modal/Comic.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { CLIENT_HEIGHT, CLIENT_WIDTH } from '../../global/sizes';
 import { FrontCoverHo, FrontCover, HO_HEIGHT, VER_HEIGHT, BannerCover, BANNER_WIDTH, BANNER_TOTAL_HEIGHT } from '../../component/frontCover/index';
 
-
 const SudokuHo_WIDTH = CLIENT_WIDTH - 24;
 const BOTTOM_BTN_WIDTH = (SudokuHo_WIDTH - 10) / 2;
 
@@ -148,24 +147,45 @@ class SudokuVe extends Component {
 const Comic2_WIDTH = CLIENT_WIDTH - 24;
 
 class Comic2 extends Component {
+
+    state = {
+        page: 1,
+        totalPage: Math.ceil(this.props.data.length / 4)
+    }
+
     render() {
+        let bannerData = this.props.data[(this.state.page - 1) * 4];
+        const totalHeight = 30 + BANNER_TOTAL_HEIGHT + VER_HEIGHT + 44 + 50;
         return (
-            <div style={{ height: 600, width: Comic2_WIDTH, display: 'flex', flexDirection: 'column' }}>
-                <div style={{ height: BANNER_TOTAL_HEIGHT, width: BANNER_WIDTH, display: 'flex', flexDirection: 'column' }}>
-                    <BannerCover />
+            <div style={{ height: totalHeight, width: Comic2_WIDTH, display: 'flex', flexDirection: 'column' }}>
+                <div style={{ marginTop: 20, height: 30, width: SudokuVe_WIDTH, display: 'flex', flexDirection: 'column', fontSize: 20, color: 'rgb(34,34,34)', fontWeight: 'bold' }}>
+                    {this.props.title}
                 </div>
-                <div style={{ marginTop: 10, height: 200, width: Comic2_WIDTH, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                <div style={{ height: BANNER_TOTAL_HEIGHT, width: BANNER_WIDTH, display: 'flex', flexDirection: 'column' }}>
+                    <BannerCover title={bannerData.title} intro={bannerData.intro} source={bannerData.cover_url} />
+                </div>
+                <div style={{ marginTop: 10, height: VER_HEIGHT, width: Comic2_WIDTH, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
                     {
-                        this.itemsGen(this.props.data, this.state.page, this.props.limit)
+                        this.itemsGen(this.props.data, this.state.page)
                     }
+                </div>
+                <div style={{ height: 44, width: SudokuVe_WIDTH, marginTop: 20, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <div onClick={this._more} style={{ height: 44, width: BOTTOM_BTN_WIDTH, backgroundColor: 'rgb(244,244,244)', borderRadius: 4, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        <div style={{ height: 14, width: 14 }}><img style={{ height: 14, width: 14 }} src={require('../../image/main/more.png')} alt='' /></div>
+                        <div style={{ fontSize: 14, color: 'rgb(152,152,152)', marginLeft: 5 }}>更多</div>
+                    </div>
+                    <div onClick={this._changePage} style={{ height: 44, width: BOTTOM_BTN_WIDTH, backgroundColor: 'rgb(244,244,244)', borderRadius: 4, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        <div style={{ height: 14, width: 14 }}><img style={{ height: 14, width: 14 }} src={require('../../image/main/change.png')} alt='' /></div>
+                        <div style={{ fontSize: 14, color: 'rgb(152,152,152)', marginLeft: 5 }}>换一批</div>
+                    </div>
                 </div>
             </div>
         );
     }
 
-    itemsGen = (data, page, limit) => {
+    itemsGen = (data, page) => {
         let result = [];
-        for (let i = limit * (page - 1); i < limit * page; i++) {
+        for (let i = 4 * (page - 1) + 1; i < 4 * page; i++) {
             if (i >= data.length) {
                 break;
             }
@@ -173,6 +193,30 @@ class Comic2 extends Component {
             result.push(<FrontCover key={i} title={item.title} intro={item.intro} source={item.cover_url} />);
         }
         return result;
+    }
+
+    _more = () => {
+        console.log('more');
+    }
+
+    _changePage = () => {
+        this.setState((preState) => {
+            let newPage = preState.page + 1;
+            if (newPage > preState.totalPage) {
+                newPage = 1;
+            }
+            return {
+                page: newPage
+            }
+        });
+    }
+}
+
+class Comic3 extends Component {
+    render() {
+        return (
+            <div />
+        );
     }
 }
 
@@ -193,7 +237,7 @@ export default class Comic extends Component {
             case 'comic_1':
                 return <SudokuVe title={this.props.title} data={this.props.data} limit={this.props.limit} />;
             case 'comic_2':
-                return <SudokuHo title={this.props.title} data={this.props.data} limit={this.props.limit} />;
+                return <Comic2 title={this.props.title} data={this.props.data} />;
             // case 'comic_3':
             //     return <SudokuVe title={this.props.title} data={this.props.data} limit={this.props.limit} />;
             // case 'comic_4':
