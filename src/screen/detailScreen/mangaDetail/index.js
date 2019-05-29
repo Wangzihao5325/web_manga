32413381ef2d970b5204c902c93d3e2eb2f5e3ee
Wangzihao5325 <416990273@qq.com>
2019-05-29 22:13:21 +0700

    1.漫画详情页 done

--- src/screen/detailScreen/mangaDetail/index.js
+++ src/screen/detailScreen/mangaDetail/index.js
@@ -9,7 +9,9 @@ import SecurtyImage from '../../../component/securtyImage/Image';
 import './index.css';
 import 'antd/dist/antd.css';
 import { Rate } from 'antd';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
 import InfiniteScroll from 'react-infinite-scroller';
+import { FrontCover, VER_WIDTH, VER_HEIGHT } from '../../../component/frontCover/index';
 
 class MangaInfoHeader extends PureComponent {
 
@@ -82,7 +84,8 @@ class MangaInfoHeader extends PureComponent {
 
 class ChapterCoverItem extends PureComponent {
     render() {
-        let dateStr = this.props.item.online_at.split(' ')[0];
+        //let dateStr = this.props.item.online_at.split(' ')[0];
+        let dateStr = this.props.item.online_at;
         let coins = Math.abs(this.props.item.coins);
         return (
             <div style={{ height: 75, width: CLIENT_WIDTH, display: 'flex', flexDirection: 'row' }}>
@@ -109,6 +112,36 @@ class ChapterCoverItem extends PureComponent {
                 }
             </div>
         );
+    }
+}
+
+const GuessLike_WIDTH = CLIENT_WIDTH - 24;
+const GuessLike_HEIGHT = VER_HEIGHT + 50;
+
+class GuessLike extends PureComponent {
+    render() {
+        const items = this.itemsGen(this.props.data);
+        return (
+            <div style={{ alignSelf: 'center', height: GuessLike_HEIGHT, width: GuessLike_WIDTH, display: 'flex', flexDirection: 'column' }}>
+                <div style={{ marginTop: 10, marginBottom: 10, height: 30, width: GuessLike_WIDTH, display: 'flex', flexDirection: 'column', fontSize: 20, color: 'rgb(34,34,34)', fontWeight: 'bold' }}>
+                    猜你喜欢
+                </div>
+                <div style={{ height: VER_HEIGHT, width: GuessLike_WIDTH }}>
+                    <ScrollMenu
+                        data={items}
+                        onSelect={this.onSelect}
+                        itemStyle={{ outline: 'none' }}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+    itemsGen = (data) => {
+        let result = data.map((item, index) => {
+            return <FrontCover key={index} title={item.title} intro={item.intro} source={item.cover_url} coverClick={() => { this.props.navi.push(`/manga_detail/${item.id}/${this.props.globalType}`) }} />;
+        });
+        return result;
     }
 }
 
@@ -144,6 +177,9 @@ class MangaDetail extends PureComponent {
         //猜你喜欢查询
         Api.guessLike(mangaId, (e) => {
             console.log(e);
+            this.setState({
+                guessLikeData: e.data
+            })
         });
     }
 
@@ -192,11 +228,16 @@ class MangaDetail extends PureComponent {
                     <div style={{ fontSize: 15, color: 'rgb(255,42,49)', marginLeft: 2 }}>展开目录</div>
                 </div>
 
-                {
+                <GuessLike navi={this.props.history} data={this.state.guessLikeData} globalType={this.props.match.params.type} />
+                <div style={{ height: 80, width: CLIENT_WIDTH }} />
+                <div style={{ backgroundColor: 'white', height: 80, width: CLIENT_WIDTH, position: 'fixed', left: 0, bottom: 0, display: 'flex', flexDirection: 'row' }}>
+                    <div><img style={{ height: 80, width: 80 }} src={require('../../../image/detail/like.png')} alt='' /></div>
+                    <div style={{ fontSize:16,color:'white',marginTop: 10, borderTopLeftRadius: 4, borderTopRightRadius: 25, borderBottomLeftRadius: 25, borderBottomRightRadius: 25, backgroundColor: 'rgb(255,42,49)', height: 50, width: 268, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                        立即阅读
+                    </div>
+                </div>
 
-                }
-
-            </div>
+            </div >
         );
     }
 
