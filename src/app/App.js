7e527e3802e816ba1b1f58797a70abb948b488bf
Wangzihao5325 <416990273@qq.com>
2019-05-31 17:21:02 +0700

    1.normal merge

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import { Provider } from 'react-redux';
 
 import Router from '../router';
 import store from '../store';
 
-function AppRouter() {
-  return (
-    <Provider store={store}>
-      <div>
-        <Router />
-        {/*process.env.NODE_ENV === 'production'?'':<DevTools/>*/}
-      </div>
-    </Provider>
-  );
+
+class AppRouter extends Component {
+
+  render() {
+    return (
+      <Provider store={store} >
+        <div>
+          <Router />
+          {/*process.env.NODE_ENV === 'production'?'':<DevTools/>*/}
+        </div>
+      </Provider>
+    );
+  }
+
 }
 
 export default AppRouter;
