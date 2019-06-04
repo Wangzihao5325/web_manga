1cc5960e5052f64cd67b21cff6e92eb6eafca300
Wangzihao5325 <416990273@qq.com>
2019-06-04 20:14:06 +0700

    1.input outline none

--- src/screen/mine/setInviteCode/index.js
+++ src/screen/mine/setInviteCode/index.js
@@ -27,7 +27,7 @@ class SetInviteCode extends Component {
             <div style={{ flex: 1 }}>
                 {this.props.invite_me_code.length === 0 && <HeaderPro rightBtnClick={this.submit} back={this.goBack} title='邀请码' rightBtnText='保存' />}
                 {this.props.invite_me_code.length > 0 && <HeaderPro back={this.goBack} title='邀请码' />}
-                {this.props.invite_me_code.length === 0 && <input onChange={this.textOnChange} style={{ fontSize: 15, marginLeft: 15, alignSelf: 'center', borderRadius: 5, borderColor: 'rgb(245,245,245)', borderStyle: 'solid', borderWidth: 1, marginTop: 18, height: 50, width: CLIENT_WIDTH - 30 }} placeholder='请填写邀请码' />}
+                {this.props.invite_me_code.length === 0 && <input onChange={this.textOnChange} style={{ outline: 'none', fontSize: 15, marginLeft: 15, alignSelf: 'center', borderRadius: 5, borderColor: 'rgb(245,245,245)', borderStyle: 'solid', borderWidth: 1, marginTop: 18, height: 50, width: CLIENT_WIDTH - 30 }} placeholder='请填写邀请码' />}
                 {this.props.invite_me_code.length > 0 &&
                     <div style={{ marginLeft: 15, borderRadius: 5, backgroundColor: 'rgb(245,245,245)', marginTop: 18, height: 50, width: CLIENT_WIDTH - 30, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                         <div style={{ fontSize: 15, marginLeft: 10, color: 'rgb(34,34,34)', fontWeight: 'bold' }}>{this.props.invite_me_code}</div>
