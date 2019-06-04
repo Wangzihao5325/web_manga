b0d2344243c8a284a35aa115de093b6cb94cfa36
Wangzihao5325 <416990273@qq.com>
2019-06-04 17:33:27 +0700

    1.邀请记录优化

--- src/screen/mine/inviteList/index.js
+++ src/screen/mine/inviteList/index.js
@@ -11,6 +11,7 @@ import './index.css';
 import Api from '../../../socket/index';
 import InfiniteScroll from 'react-infinite-scroller';
 
+const BG_WIDTH = CLIENT_WIDTH > 347 ? 347 : CLIENT_WIDTH - 28;
 
 class InviteList extends PureComponent {
 
@@ -31,13 +32,17 @@ class InviteList extends PureComponent {
 
     render() {
         const listItems = this.state.data.map((item, index) => {
+            let time = item.created_at;
+            if (time.indexOf(' ') > 0) {
+                time = time.split(' ')[0];
+            }
             return (
-                <div key={index} style={{ borderBottomStyle: 'solid', borderBottomColor: 'rgb(244,244,244)', borderBottomWidth: 1, alignSelf: 'center', height: 50, width: CLIENT_WIDTH - 28, display: 'flex', flexDirection: 'row' }}>
+                <div key={index} style={{ marginLeft: 14, borderBottomStyle: 'solid', borderBottomColor: 'rgb(244,244,244)', borderBottomWidth: 1, alignSelf: 'center', height: 50, width: CLIENT_WIDTH - 28, display: 'flex', flexDirection: 'row' }}>
                     <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
                         {`${item.nick_name}`}
                     </div>
                     <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                        {`${item.created_at}`}
+                        {`${time}`}
                     </div>
                 </div>
             );
@@ -45,7 +50,7 @@ class InviteList extends PureComponent {
         return (
             <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }} >
                 <HeaderPro title='邀请记录' back={this.goBack} />
-                <div className='mine-header-container' style={{ alignSelf: 'center', marginTop: 23, height: 135, width: 347, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${image_bg})` }}>
+                <div className='mine-header-container' style={{ borderRadius: 10, alignSelf: 'center', marginTop: 23, height: 135, width: BG_WIDTH, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${image_bg})` }}>
                     <div style={{ fontSize: 35, color: 'white' }}>{this.props.invite}</div>
                     <div style={{ fontSize: 14, color: 'white', marginTop: 6 }}>已邀请人数</div>
                 </div>
