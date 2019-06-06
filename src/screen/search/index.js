2910f4b599ce6ade71dd9668449b751fd96e4cf9
Wangzihao5325 <416990273@qq.com>
2019-06-06 21:39:42 +0700

    1.热门搜索跳转

--- src/screen/search/index.js
+++ src/screen/search/index.js
@@ -6,17 +6,56 @@ import { HeaderPro } from '../../component/header/index';
 import { CLIENT_WIDTH, CLIENT_HEIGHT } from '../../global/sizes';
 import { ToastsStore } from 'react-toasts';
 import Api from '../../socket/index';
+import './index.css';
 
 const textReg = { content: '' };
+
+class KeyWordsItem extends PureComponent {
+
+    static defaultProps = {
+        title: '    ',
+        value: ''
+    }
+
+    render() {
+        let color = 'rgb(168,168,168)';
+        let textColor = 'rgb(34,34,34)';
+        let length = this.props.title.length * 13 + 24;
+        return (
+            <div onClick={this.itemSelect} style={{ marginTop: 10, marginLeft: 5, marginRight: 5, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: 34, width: length, borderRadius: 17, backgroundColor: 'white', borderColor: color, borderStyle: 'solid', borderWidth: 1 }}>
+                <div className='text_div' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginLeft: 12, marginRight: 12, color: textColor, fontSize: 13, }}>
+                    {this.props.title}
+                </div>
+            </div>
+        );
+    }
+
+    itemSelect = () => {
+        if (this.props.clickCallback) {
+            this.props.clickCallback(this.props.item);
+        }
+    }
+}
 
 class Search extends PureComponent {
 
     state = {
-        isSearch: false
+        isSearch: false,
+        hotData: []
     }
 
     componentDidMount() {
         store.dispatch(tab_navi_unshow());
+        const type = this.props.match.params.type;
+        Api.guessLike(type, 1, (e) => {
+            let data = e.data;
+            if (data.length > 6) {
+                data.length = 6;
+            }
+            this.setState({
+                hotData: data
+            });
+        });
     }
 
     render() {
@@ -34,11 +73,31 @@ class Search extends PureComponent {
                 </div>
 
                 {
+
                     !this.state.isSearch &&
-                    <div></div>
+                    <div style={{ marginTop: 10, alignSelf: 'center', display: 'flex', height: 30, width: CLIENT_WIDTH - 14, flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={{ marginLeft: 12, color: 'rgb(168,168,168)', fontSize: 14 }}>热门搜索</div>
+                        <div ></div>
+                    </div >
+                }
+
+                {
+
+                    !this.state.isSearch &&
+                    <div style={{ alignSelf: 'center', display: 'flex', width: CLIENT_WIDTH - 14, flexDirection: 'row', justifyContent: 'flex-start', flexWrap: 'wrap' }}>
+                        {
+                            this.state.hotData.map((item, index) => {
+                                return <KeyWordsItem clickCallback={this.itemClick} key={index} item={item} value={item.id} title={item.title} />
+                            })
+                        }
+                    </div >
                 }
             </div>
         );
+    }
+
+    itemClick = (item) => {
+        this.props.history.push(`/manga_detail/${item.id}/${item.global_type}`)
     }
 
     inputOnChange = ({ target }) => {
