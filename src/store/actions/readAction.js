52f8d619b4e7db77d525dc565a505a8f50d82004
Wangzihao5325 <416990273@qq.com>
2019-06-07 22:56:49 +0700

    1.store优化

--- src/store/actions/readAction.js
+++ src/store/actions/readAction.js
@@ -1,9 +1,13 @@
 import * as Types from '../actionTypes';
 
-export function test_add() {
-    return { type: Types.TEST_INCREMENT };
+export function auto_buy_true() {
+    return { type: Types.READ_AUTO_BUY_TRUE };
 }
 
-export function test_min() {
-    return { type: Types.TEST_DECREMENT };
+export function auto_buy_false() {
+    return { type: Types.READ_AUTO_BUY_FALSE };
+}
+
+export function auto_buy_change() {
+    return { type: Types.READ_AUTO_BUY_CHANGE };
 }
